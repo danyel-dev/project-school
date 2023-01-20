009c2098b744dc6594aee73c0b5d1adbfb7628dc
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 <danyel-dev>
2023-01-20 17:35:19 -0300

    routers created

--- escola/views.py
+++ escola/views.py
@@ -1,15 +1,15 @@
 from rest_framework import viewsets
 from .models import Aluno, Curso
-from serializer import AlunoSerializer, CursoSerializer
+from .serializer import AlunoSerializer, CursoSerializer
 
 
-class AlunosSerializer(viewsets.ModelViewSet):
+class AlunosViewset(viewsets.ModelViewSet):
     """Exibindo todos os alunos"""
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
 
 
-class CursosSerializer(viewsets.ModelViewSet):
+class CursosViewset(viewsets.ModelViewSet):
     """Exibindo todos os alunos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
