7941128b254ebe47ba05deb1c2a818043a9c9cde
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 <danyel-dev>
2023-01-20 16:29:12 -0300

    viewset AlunosSerializer and CursosSerializers created

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Aluno, Curso
+from serializer import AlunoSerializer, CursoSerializer
 
-# Create your views here.
+
+class AlunosSerializer(viewsets.ModelViewSet):
+    """Exibindo todos os alunos"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class CursosSerializer(viewsets.ModelViewSet):
+    """Exibindo todos os alunos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
